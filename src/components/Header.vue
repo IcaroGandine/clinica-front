04d7272409bbec36ae029bebb16e1fed94f9975d
Icaro Weiler <icarog28@gmail.com>
2023-08-22 12:45:01 -0300

    ffeat(header): cria layout para header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <header>
+    <div>test</div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background: white;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
